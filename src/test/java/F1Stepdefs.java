9c96614130af74ff0307983639c8f370a12a7d26
Govindasamy <govindasamy.jayabalan@outlook.com>
2019-05-16 15:16:09 +0100

    homepage

--- src/test/java/F1Stepdefs.java
+++ src/test/java/F1Stepdefs.java
@@ -1,10 +1,20 @@
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class F1Stepdefs {
+    private WebDriver driver;
+
+    public void getDriverManager() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gopi Jayabalan\\Downloads\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+
     @Given("^Go to the landing page$")
     public void goToTheLandingPage() {
 
+        driver.get("http://ergast.com/api/f1/2017/circuits.json");
     }
 
     @And("^I enter the circuit id$")
